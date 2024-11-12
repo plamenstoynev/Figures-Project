c9d1635c7f2479dea8392801246fafc0b9be02d6
plamenstoynev <plamen.stoynev24@gmail.com>
2024-11-12 09:37:56 +0200

    Combine factories tests in one

--- tests/FactoriesTests.cpp
+++ tests/FactoriesTests.cpp
@@ -1,4 +1,3 @@
-#include "Rectangle.h"
 #include "catch2/catch_all.hpp"
 #include "../Factories/AbstractFactory.h"
 #include "../Factories/FigureFactory.h"
@@ -11,4 +10,20 @@ TEST_CASE("Abstract Factory tests") {
 
     REQUIRE_THROWS(factory.chooseFactory("stream", nullptr));
     REQUIRE_THROWS(factory.chooseFactory("unknown"));
+}
+
+TEST_CASE("RandomFactory create") {
+    RandomFactory randomFactory;
+    REQUIRE_NOTHROW(randomFactory.create());
+}
+
+TEST_CASE("Stream constructor") {
+    std::istringstream input("Circle 5\nRectangle 3 4\nTriangle 3 4 5");
+    StreamFactory stream(input);
+    REQUIRE_NOTHROW(stream.create());
+    REQUIRE_NOTHROW(stream.create());
+    REQUIRE_NOTHROW(stream.create());
+
+    std::stringstream emptyInput;
+    REQUIRE_THROWS(StreamFactory(emptyInput));
 }
--- tests/RandomFactoryTests.cpp
+++
@@ -1,7 +0,0 @@
-#include "catch2/catch_all.hpp"
-#include "../Factories/RandomFactory.h"
-
-TEST_CASE("RandomFactory create") {
-    RandomFactory randomFactory;
-    REQUIRE_NOTHROW(randomFactory.create());
-}

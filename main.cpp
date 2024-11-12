b60bf0314319af8cda8596fab48837be62dcad75
plamenstoynev <plamen.stoynev24@gmail.com>
2024-11-12 18:28:53 +0200

    make it cool

--- main.cpp
+++ main.cpp
@@ -12,35 +12,50 @@ int main() {
     FigureFactory factory;
     std::vector<Figure*> figures;
     stringConvertible* stringConverter;
-    size_t index = 0;
     std::ofstream file("figures.txt");
 
-    while(command != "exit" && index < 3) {
-        std::cout << "Enter command: (random or stream)";
+    std::cout << "Rules for entering data and commands \n"
+                    "random - to create a random figure\n"
+                    "stream - to create a figure from the stream\n"
+                    "exit - to exit the program\n"
+                    "For example, to create a triangle, enter 'stream Triangle 3 4 5'\n"
+                    "For example, to create a rectangle, enter 'stream Rectangle 3 4'\n"
+                    "For example, to create a circle, enter 'stream Circle 3"<<
+                    std::endl;
+
+
+    while(command != "exit") {
+        std::cout << "Enter command: ";
         std::cin >> command;
-        if(command == "random") {
+
+        if(command == "random")
             figures.push_back(factory.chooseFactory("random")->create());
-            index++;
-        }
+
         else if(command == "stream") {
             std::cout << "Enter figure data: ";
             std::cin.ignore();
             std::getline(std::cin, command);
             std::istringstream input(command);
-            figures.push_back(factory.chooseFactory("stream", &input)->create());
-            index++;
+            //use try catch block to catch exceptions
+            try {
+                figures.push_back(factory.chooseFactory("stream", &input)->create());
+            } catch (std::invalid_argument& e) {
+                std::cout << e.what() << std::endl;
+            }
         }
+
         else if(command == "exit") {
             break;
         }
+
         else {
             std::cout << "Unknown command" << std::endl;
             continue;
         }
     }
 
-    for (size_t i = 0; i < index; i++) {
-        stringConverter = dynamic_cast<stringConvertible*>(figures[i]);
+    for (auto it : figures) {
+        stringConverter = dynamic_cast<stringConvertible*>(it);
         if (stringConverter != nullptr) {
             std::cout << stringConverter->toString() << std::endl;
 
